192c176f23d11a5ebd9ea5c1130e1adb570366fd
Sergio Demarco <sergiodemarco@selehann.com>
2023-04-10 20:40:43 -0400

    Code

---
+++ NovatechApp/Controllers/NovatechController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net;
+
+namespace NovatechApp.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	[EnableCors()]
+	public class NovatechController : ControllerBase
+	{
+
+		[HttpGet]
+		public string Get()
+		{
+			var url = "https://gorest.co.in/public/v2/users";
+			WebRequest request = HttpWebRequest.Create(url);
+			WebResponse response = request.GetResponse();
+			StreamReader reader = new StreamReader(response.GetResponseStream());
+			string responseText = reader.ReadToEnd();
+			return responseText;
+		}
+	}
+}
